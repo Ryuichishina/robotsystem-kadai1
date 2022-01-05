283dae5c18e190799560acda66e9dda4c9559c71
Ryuichi shina <s20C1052bu@s.chibakoudai.jp>
2022-01-05 16:24:40 +0000

    Update

--- myled.mod.c
+++ myled.mod.c
@@ -34,11 +34,19 @@ __used __section(__versions) = {
 	{ 0x21872aaa, "cdev_add" },
 	{ 0xe7bd1d8b, "cdev_init" },
 	{ 0xe3ec2f2b, "alloc_chrdev_region" },
+	{ 0x6cbbfc54, "__arch_copy_to_user" },
+	{ 0xdecd0b29, "__stack_chk_fail" },
+	{ 0xd697e69a, "trace_hardirqs_on" },
 	{ 0xc5850110, "printk" },
+	{ 0x12a4e128, "__arch_copy_from_user" },
+	{ 0xec3d2e1b, "trace_hardirqs_off" },
+	{ 0x3d8560e4, "cpu_hwcaps" },
+	{ 0x4b50cb71, "cpu_hwcap_keys" },
+	{ 0x14b89635, "arm64_const_caps_ready" },
 	{ 0x1fdc7df2, "_mcount" },
 };
 
 MODULE_INFO(depends, "");
 
 
-MODULE_INFO(srcversion, "EDABBB64D0FC19BECFA8EC7");
+MODULE_INFO(srcversion, "8525D7939E229EA6BD4DEFC");
