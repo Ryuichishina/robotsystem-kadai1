e7eeca7c3d2c6b8501f2a89450d2ebd6ec4752d2
Ryuichi shina <s20C1052bu@s.chibakoudai.jp>
2022-01-07 04:43:04 +0000

    Update

--- myled.c
+++ myled.c
@@ -13,6 +13,18 @@ MODULE_VERSION("0.0.1");
  static dev_t dev;
  static struct cdev cdv;
 
+static ssize_t led_write(struct file*filp, const char*buf, size_t count, loff_t*pos)
+{
+	printk(KERN_INFO "led_write is called\n");
+	return 1;
+}
+
+static struct file_operations led_fops = {
+	.owner = THIS_MODULE,
+	.write = led_write
+};
+
+
  static int __init init_mod(void)
 {
 	int retval;
@@ -24,11 +36,19 @@ MODULE_VERSION("0.0.1");
 	}
 
 	printk(KERN_INFO "%s is loaded. major : %d\n", __FILE__, MAJOR(dev));
+	
+	cdev_init(&cdv, &led_fops);
+	retval	= cdev_add(&cdv, dev, 1);
+	if(retval < 0){
+		printk(KERN_ERR "cdev_add failed. major %d, minor: %d\n", MAJOR(dev),MINOR(dev));
+	}
+	
 	return 0;
 }
 
 static void __exit cleanup_mod(void)
 {
+	cdev_del(&cdv);
 	unregister_chrdev_region(dev,1);
 	printk(KERN_INFO "%s is unloaded. major: %d\n",__FILE__ ,MAJOR(dev));
 }
--- myled.mod.c
+++ myled.mod.c
@@ -26,12 +26,15 @@ static const struct modversion_info ____versions[]
 __used __section(__versions) = {
 	{ 0xfe9bc451, "module_layout" },
 	{ 0x6091b333, "unregister_chrdev_region" },
-	{ 0xc5850110, "printk" },
+	{ 0x58055dbb, "cdev_del" },
+	{ 0x21872aaa, "cdev_add" },
+	{ 0xe7bd1d8b, "cdev_init" },
 	{ 0xe3ec2f2b, "alloc_chrdev_region" },
+	{ 0xc5850110, "printk" },
 	{ 0x1fdc7df2, "_mcount" },
 };
 
 MODULE_INFO(depends, "");
 
 
-MODULE_INFO(srcversion, "B4FA94A8FD8B0091CA9CB49");
+MODULE_INFO(srcversion, "805E024C18C1E0194E8E6E8");
